f1f1bb46349d9ebdba979f7f3f6edea07476572e
honam <75104291+honambsn@users.noreply.github.com>
2024-02-27 23:10:38 +0700

    fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ openai.api_key = "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"
 
 def chat_w_gpt(prompt):
   response = openai.ChatCompletion.create(
-    model = "ft:gpt-3.5-turbo-0613:personal::8wBiYAPJ", eng-ver.
+    model = "ft:gpt-3.5-turbo-0613:personal::8wBiYAPJ", #eng-ver.
     #model = "ft:gpt-3.5-turbo-0613:personal::8wtkKqNW", vn ver.
     messages = [{"role": "user", "content": prompt}]
   )
